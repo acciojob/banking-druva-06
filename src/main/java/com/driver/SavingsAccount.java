9d27e1a19ab5f21c3b0612752d5939c327809598
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-24 01:45:14 +0530

    Modified rate of Intrest in Savings Account

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -15,7 +15,7 @@ public class SavingsAccount extends BankAccount{
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
-        if(amount > maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
+        if(amount >= maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
         super.withdraw(amount);
     }
 
